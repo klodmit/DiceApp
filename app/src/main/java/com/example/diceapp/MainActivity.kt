9cdf6b43f37479fa100c6e8a79abcb0f5624d66f
klodmit <klochixindima8@gmail.com>
2023-11-22 01:25:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.diceapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diceapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.toRoll.setOnClickListener {
+            rollDice()
+        }
+        setContentView(binding.root)
+    }
+
+    fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+
+        binding.diceView.setImageResource(drawableResource)
+        binding.diceView.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+
+}
+
+
